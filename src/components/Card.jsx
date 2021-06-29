a5015c3ce25687b97758dfd4f922f66e25c65259
lfernanda2504 <luizafernandagarcia@gmail.com>
2021-06-29 11:45:21 -0500

    agregando estilo card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { Card } from  'react-bootstrap' ;
 
 const Card = () => {
   return (
     <div>
-      <p>Contenido de la card</p>
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Card.Title>Pokemon </Card.Title>
+          <Card.Text>
+          <p>Nombre</p>
+          </Card.Text>
+          <Button variant="primary">Ver mas</Button>
+        </Card.Body>
+      </Card>
     </div>
   );
 };
