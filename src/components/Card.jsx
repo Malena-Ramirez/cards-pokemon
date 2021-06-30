e00de16e82a76c90cf5fbc561e3a4375a0bb7bd0
jorgeQ <alejandrojj2018@gmail.com>
2021-06-29 23:03:41 -0500

    agregando peticion a api

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,13 @@
-import React from "react";
-
-const Card = () => {
-  return (
-    <div>
-      <p>Contenido de la card</p>
-    </div>
-  );
-};
-
+const Card = ({name,img,abilities,exp}) =>{
+  return(
+      <div>
+          <figure>
+          <img src={img} alt={name} />
+          <figcaption>{name}</figcaption>
+          </figure>
+          <h3>Habilidad : {abilities}</h3>
+          <h3>Exp : {exp}</h3>
+      </div>
+  )
+}
 export default Card;
